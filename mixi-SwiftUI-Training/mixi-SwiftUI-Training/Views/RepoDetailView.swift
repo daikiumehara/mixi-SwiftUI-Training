5a0badd6b63936a1e8aa70dba7dd71431a1e7938
daikiumehara <daikiumehara3@gmail.com>
2022-06-11 02:53:29 +0900

    詳細画面を作成しました

--- mixi-SwiftUI-Training/mixi-SwiftUI-Training/Views/RepoDetailView.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/Views/RepoDetailView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct RepoDetailView: View {
+    var repo: Repo
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                Image("GitHubMark")
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                
+                Text(repo.owner.name)
+                    .font(.caption)
+            }
+            
+            Text(repo.name)
+                .bold()
+            
+            Text(repo.description)
+                .padding(.top, 4)
+            
+            HStack {
+                Image(systemName: "star")
+                
+                Text("\(repo.stargazersCount) stars")
+            }
+            .padding(.top, 8)
+        }
     }
 }
 
 struct RepoDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        RepoDetailView()
+        RepoDetailView(repo: .mock1)
     }
 }
