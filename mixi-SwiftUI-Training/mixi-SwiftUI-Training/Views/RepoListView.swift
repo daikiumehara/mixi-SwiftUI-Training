4a562e57489bbe3ea6b7cc354f75177cd4c37d7f
daikiumehara <daikiumehara3@gmail.com>
2022-06-11 02:34:20 +0900

    ファイルを追加、フォルダ名を変更しました

--- mixi-SwiftUI-Training/mixi-SwiftUI-Training/Views/RepoListView.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/Views/RepoListView.swift
@@ -8,25 +8,17 @@
 import SwiftUI
 
 struct RepoListView: View {
-    @StateObject private var reposStore = ReposStore()
+    private let mockRepos = [
+        Repo(id: 1, name: "Test Repo1", owner: User(name: "Test User1"), description: "This is a good code sample", stargazersCount: 10),
+        Repo(id: 2, name: "Test Repo2", owner: User(name: "Test User2"), description: "This is a good code sample", stargazersCount: 10),
+        Repo(id: 3, name: "Test Repo3", owner: User(name: "Test User3"), description: "This is a good code sample", stargazersCount: 10),
+        Repo(id: 4, name: "Test Repo4", owner: User(name: "Test User4"), description: "This is a good code sample", stargazersCount: 10),
+        Repo(id: 5, name: "Test Repo5", owner: User(name: "Test User5"), description: "This is a good code sample", stargazersCount: 10)
+    ]
     
     var body: some View {
-        NavigationView {
-            if reposStore.repos.isEmpty {
-                ProgressView("loading...")
-            } else {
-                List(reposStore.repos) { repo in
-                    NavigationLink {
-                        RepoDetailView(repo: repo)
-                    } label: {
-                        RepoRow(repo: repo)
-                    }
-                }
-                .navigationTitle("Repositories")
-            }
-        }
-        .task {
-            await reposStore.loadRepos()
+        List(mockRepos) { repo in
+            RepoRow(repo: repo)
         }
     }
 }
