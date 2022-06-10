f23e11b94d04f9b91a785b67fb5f64e6f4da4e42
daikiumehara <daikiumehara3@gmail.com>
2022-06-11 02:34:20 +0900

    ファイルを追加、フォルダ名を変更しました

---
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/Views/RepoDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  RepoDetailView.swift
+//  mixi-SwiftUI-Training
+//
+//  Created by 梅原 奈輝 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct RepoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RepoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RepoDetailView()
+    }
+}
---
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/Views/RepoListView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  mixi-SwiftUI-Training
+//
+//  Created by 梅原 奈輝 on 2022/06/10.
+//
+
+import SwiftUI
+
+struct RepoListView: View {
+    private let mockRepos = [
+        Repo(id: 1, name: "Test Repo1", owner: User(name: "Test User1"), description: "This is a good code sample", stargazersCount: 10),
+        Repo(id: 2, name: "Test Repo2", owner: User(name: "Test User2"), description: "This is a good code sample", stargazersCount: 10),
+        Repo(id: 3, name: "Test Repo3", owner: User(name: "Test User3"), description: "This is a good code sample", stargazersCount: 10),
+        Repo(id: 4, name: "Test Repo4", owner: User(name: "Test User4"), description: "This is a good code sample", stargazersCount: 10),
+        Repo(id: 5, name: "Test Repo5", owner: User(name: "Test User5"), description: "This is a good code sample", stargazersCount: 10)
+    ]
+    
+    var body: some View {
+        List(mockRepos) { repo in
+            RepoRow(repo: repo)
+        }
+    }
+}
+
+struct RepoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RepoListView()
+    }
+}
