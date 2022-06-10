19d868bcdf5469d556087c581282b39099aa90c4
daikiumehara <daikiumehara3@gmail.com>
2022-06-11 02:24:00 +0900

    repoにプロパティを追加しました

--- mixi-SwiftUI-Training/mixi-SwiftUI-Training/View/ContentView.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/View/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     private let mockRepos = [
-        Repo(id: 1, name: "Test Repo1", owner: User(name: "Test User1")),
-        Repo(id: 2, name: "Test Repo2", owner: User(name: "Test User2")),
-        Repo(id: 3, name: "Test Repo3", owner: User(name: "Test User3")),
-        Repo(id: 4, name: "Test Repo4", owner: User(name: "Test User4")),
-        Repo(id: 5, name: "Test Repo5", owner: User(name: "Test User5"))
+        Repo(id: 1, name: "Test Repo1", owner: User(name: "Test User1"), description: "This is a gool code sample", stargazersCount: 10),
+        Repo(id: 2, name: "Test Repo2", owner: User(name: "Test User2"), description: "This is a gool code sample", stargazersCount: 10),
+        Repo(id: 3, name: "Test Repo3", owner: User(name: "Test User3"), description: "This is a gool code sample", stargazersCount: 10),
+        Repo(id: 4, name: "Test Repo4", owner: User(name: "Test User4"), description: "This is a gool code sample", stargazersCount: 10),
+        Repo(id: 5, name: "Test Repo5", owner: User(name: "Test User5"), description: "This is a gool code sample", stargazersCount: 10)
     ]
     
     var body: some View {
