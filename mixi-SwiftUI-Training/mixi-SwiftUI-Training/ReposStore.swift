6e841b77f8d0196521ed656934f91912cf1c170b
daikiumehara <daikiumehara3@gmail.com>
2022-06-13 16:17:57 +0900

    storeにプロパティを追加しました

--- mixi-SwiftUI-Training/mixi-SwiftUI-Training/ReposStore.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/ReposStore.swift
@@ -10,14 +10,19 @@ import Foundation
 @MainActor
 class ReposStore: ObservableObject {
     @Published private(set) var repos = [Repo]()
+    @Published private(set) var error: APIError?
+    @Published private(set) var isLoading: Bool = false
     
     func loadRepos() async {
+        isLoading = true
         let result = await GitHubClient.fetchData()
         switch result {
         case .success(let repos):
             self.repos = repos
+            error = nil
         case .failure(let error):
-            print(error)
+            self.error = error
         }
+        isLoading = false
     }
 }
