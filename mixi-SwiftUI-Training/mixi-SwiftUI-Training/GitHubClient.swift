822f3ed020eb92395a70d8725f51060481884765
daikiumehara <daikiumehara3@gmail.com>
2022-06-13 15:02:05 +0900

    GitHubClientとAPIErrorを定義しました

---
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/GitHubClient.swift
@@ -0,0 +1,31 @@
+//
+//  GitHubClient.swift
+//  mixi-SwiftUI-Training
+//
+//  Created by 梅原 奈輝 on 2022/06/13.
+//
+
+import Foundation
+
+struct GitHubClient {
+    static func fetchData() async -> Result<[Repo], Error> {
+        let url = URL(string: "https://api.github.com/orgs/mixigroup/repos")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+        
+        guard let (data, response) = try? await URLSession.shared.data(for: request) else {
+            return .failure(APIError.unexpected)
+        }
+        guard let response = response as? HTTPURLResponse,
+              response.statusCode == 200 else {
+            return .failure(APIError.badResponse)
+        }
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        guard let repos = try? decoder.decode([Repo].self, from: data) else {
+            return .failure(APIError.missDecode)
+        }
+        return .success(repos)
+    }
+}
