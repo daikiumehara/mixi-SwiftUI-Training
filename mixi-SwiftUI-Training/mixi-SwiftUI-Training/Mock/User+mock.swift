0441bbd246f4f72f31ef9b3cc00e9fa60b0c48e6
daikiumehara <daikiumehara3@gmail.com>
2022-06-15 03:02:27 +0900

    mockのURLを修正しました

--- mixi-SwiftUI-Training/mixi-SwiftUI-Training/Mock/User+mock.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/Mock/User+mock.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 extension User {
-    static let mock1 = User(name: "Test User1", imageURL: URL(string: "")!)
-    static let mock2 = User(name: "Test User2", imageURL: URL(string: "")!)
-    static let mock3 = User(name: "Test User3", imageURL: URL(string: "")!)
-    static let mock4 = User(name: "Test User4", imageURL: URL(string: "")!)
-    static let mock5 = User(name: "Test User5", imageURL: URL(string: "")!)
+    static let mock1 = User(name: "Test User1", imageURL: URL(string: "https://github.com/daikiumehara/mixi-SwiftUI-Training/")!)
+    static let mock2 = User(name: "Test User2", imageURL: URL(string: "https://github.com/daikiumehara/mixi-SwiftUI-Training/")!)
+    static let mock3 = User(name: "Test User3", imageURL: URL(string: "https://github.com/daikiumehara/mixi-SwiftUI-Training/")!)
+    static let mock4 = User(name: "Test User4", imageURL: URL(string: "https://github.com/daikiumehara/mixi-SwiftUI-Training/")!)
+    static let mock5 = User(name: "Test User5", imageURL: URL(string: "https://github.com/daikiumehara/mixi-SwiftUI-Training/")!)
 }
