7a6cb9d077360908f804679aedc975cb0c5266eb
daikiumehara <daikiumehara3@gmail.com>
2022-06-15 11:30:36 +0900

    viewModelの中身を定義しました

--- mixi-SwiftUI-Training/mixi-SwiftUI-Training/ReposStore.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/ReposStore.swift
@@ -13,6 +13,17 @@ class ReposStore: ObservableObject {
     func loadRepos() async {
         try! await Task.sleep(nanoseconds: 1_000_000_000)
         
+<<<<<<< HEAD
         repos = [.mock1, .mock2, .mock3, .mock4, .mock5]
+=======
+        let result = await GitHubClient.fetchData()
+        switch result {
+        case .success(let repos):
+            state = .loaded(repos)
+        case .failure(let error):
+            state = .failed(error)
+        }
+        
+>>>>>>> 9981bbe (viewModelの中身を定義しました)
     }
 }
