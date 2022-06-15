15a732d3ec7a6fe972bcca168afb132f304fcd34
daikiumehara <daikiumehara3@gmail.com>
2022-06-15 17:33:34 +0900

    異常系のテストを記述しました

--- mixi-SwiftUI-Training/mixi-SwiftUI-TrainingTests/RepoListViewModelTests.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-TrainingTests/RepoListViewModelTests.swift
@@ -11,32 +11,33 @@ import XCTest
 class RepoListViewModelTests: XCTestCase {
     @MainActor func test_onAppear_正常系() {
         let viewModel = RepoListViewModel(
-            repoRepository: MockRepoRepository(repos: [.mock1, .mock2])
+            repoRepository: StubRepoRepository(state: .loaded([.mock1, .mock2]))
         )
         
-        viewModel.onAppear()
+        DispatchQueue.main.async {
+            viewModel.onAppear()
+            switch viewModel.state {
+            case let .loaded(repos):
+                XCTAssertEqual(repos, [Repo.mock1, Repo.mock2])
+            default:
+                XCTFail()
+            }
+        }
+    }
+    
+    @MainActor func test_onAppear_異常系() {
+        let viewModel = RepoListViewModel(
+            repoRepository: StubRepoRepository(state: .failed(.badResponse))
+        )
         
-        switch viewModel.state {
-        case let .loaded(repos):
-            XCTAssertEqual(repos, [Repo.mock1, Repo.mock2])
-        default:
-            XCTFail()
+        DispatchQueue.main.async {
+            viewModel.onAppear()
+            switch viewModel.state {
+            case let .failed(error):
+                XCTAssertEqual(error, APIError.badResponse)
+            default:
+                XCTFail()
+            }
         }
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
