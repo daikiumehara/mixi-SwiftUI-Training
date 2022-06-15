330d5fa2bd1bfeb584f541d9066d440c2e84df9b
daikiumehara <daikiumehara3@gmail.com>
2022-06-15 11:17:30 +0900

    repositoryとViewModelを作成しました

---
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/Repositories/RepoRepository.swift
@@ -0,0 +1,12 @@
+//
+//  RepoRepository.swift
+//  mixi-SwiftUI-Training
+//
+//  Created by 梅原 奈輝 on 2022/06/15.
+//
+
+import Foundation
+
+final class RepoRepository {
+    
+}
---
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/ViewModels/RepoListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RepoListViewModel.swift
+//  mixi-SwiftUI-Training
+//
+//  Created by 梅原 奈輝 on 2022/06/15.
+//
+
+import Foundation
+
+final class RepoListViewModel {
+    
+}
