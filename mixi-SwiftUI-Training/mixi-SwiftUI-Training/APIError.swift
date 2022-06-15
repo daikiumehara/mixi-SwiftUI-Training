847d50a9557d83013c15645bd9ddb6fff82cf235
daikiumehara <daikiumehara3@gmail.com>
2022-06-16 03:19:31 +0900

    正しくテストできるように修正しました

--- mixi-SwiftUI-Training/mixi-SwiftUI-Training/APIError.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/APIError.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 
-enum APIError: Error {
+enum APIError: Error, Equatable {
     case decodingError
     case badResponse
     case unexpected(_ error: Error)
+    
+    static func == (lhs: APIError, rhs: APIError) -> Bool {
+        lhs.localizedDescription == rhs.localizedDescription
+    }
 }
--- mixi-SwiftUI-Training/mixi-SwiftUI-TrainingTests/RepoListViewModelTests.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-TrainingTests/RepoListViewModelTests.swift
@@ -6,38 +6,109 @@
 //
 
 import XCTest
+import Combine
 @testable import mixi_SwiftUI_Training
 
 class RepoListViewModelTests: XCTestCase {
-    @MainActor func test_onAppear_正常系() {
-        let viewModel = RepoListViewModel(
-            repoRepository: StubRepoRepository(state: .loaded([.mock1, .mock2]))
-        )
-        
-        DispatchQueue.main.async {
-            viewModel.onAppear()
-            switch viewModel.state {
-            case let .loaded(repos):
-                XCTAssertEqual(repos, [Repo.mock1, Repo.mock2])
-            default:
-                XCTFail()
-            }
+    var cancellables = [AnyCancellable]()
+    
+    override func tearDownWithError() throws {
+        cancellables.forEach {
+            $0.cancel()
         }
     }
     
+    @MainActor func test_onAppear_正常系() {
+        let exp = makeExpectation(#function)
+        let viewModel = RepoListViewModel(
+            repoRepository: StubRepoRepository(state: .loaded([.mock1, .mock2]))
+        )
+        var counter = 0
+        
+        let cancellable = viewModel.$state
+            .sink { state in
+                counter += 1
+                switch counter {
+                case 1:
+                    switch state {
+                    case .idle:
+                        break
+                    default:
+                        XCTFail()
+                    }
+                case 2:
+                    switch state {
+                    case .loading:
+                        break
+                    default:
+                        XCTFail()
+                    }
+                case 3:
+                    switch state {
+                    case let .loaded(repos):
+                        XCTAssertEqual(repos, [Repo.mock1, Repo.mock2])
+                        exp.fulfill()
+                    default:
+                        XCTFail()
+                    }
+                default:
+                    XCTFail()
+                }
+            }
+        
+        cancellables.append(cancellable)
+        viewModel.onAppear()
+        wait(for: [exp], timeout: 5.0)
+    }
+    
     @MainActor func test_onAppear_異常系() {
+        let exp = makeExpectation(#function)
         let viewModel = RepoListViewModel(
             repoRepository: StubRepoRepository(state: .failed(.badResponse))
         )
+        var counter = 0
         
-        DispatchQueue.main.async {
-            viewModel.onAppear()
-            switch viewModel.state {
-            case let .failed(error):
-                XCTAssertEqual(error, APIError.badResponse)
-            default:
-                XCTFail()
+        let cancellable = viewModel.$state
+            .sink { state in
+                counter += 1
+                switch counter {
+                case 1:
+                    switch state {
+                    case .idle:
+                        break
+                    default:
+                        XCTFail()
+                    }
+                case 2:
+                    switch state {
+                    case .loading:
+                        break
+                    default:
+                        XCTFail()
+                    }
+                case 3:
+                    switch state {
+                    case let .failed(error):
+                        if error == .badResponse {
+                            XCTAssertTrue(true)
+                        } else {
+                            XCTFail()
+                        }
+                        exp.fulfill()
+                    default:
+                        XCTFail()
+                    }
+                default:
+                    XCTFail()
+                }
             }
-        }
+        
+        cancellables.append(cancellable)
+        viewModel.onAppear()
+        wait(for: [exp], timeout: 5.0)
+    }
+    
+    private func makeExpectation(_ funcName: String) -> XCTestExpectation {
+        return XCTestExpectation(description: "\(funcName)のタイムアウト")
     }
 }
