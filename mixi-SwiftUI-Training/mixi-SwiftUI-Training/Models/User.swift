845b848b4883f5bb44812b6d6a350b45f4fb6fca
daikiumehara <daikiumehara3@gmail.com>
2022-06-17 15:38:50 +0900

    消しすぎていた部分を追加

--- mixi-SwiftUI-Training/mixi-SwiftUI-Training/Models/User.swift
+++ mixi-SwiftUI-Training/mixi-SwiftUI-Training/Models/User.swift
@@ -10,4 +10,9 @@ import Foundation
 struct User: Codable {
     var name: String
     var imageURL: URL
+    
+    private enum CodingKeys: String, CodingKey {
+        case name = "login"
+        case imageURL = "avatarUrl"
+    }
 }
